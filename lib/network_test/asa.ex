b861aa0be11f7d06a4ae048aac3165f89d09372c
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-01-30 11:40:17 -0600

    Place macros atop a functional interface

--- lib/network_test/asa.ex
+++ lib/network_test/asa.ex
@@ -1,4 +1,11 @@
+# Copyright © 2016 Jonathan Storm <the.jonathan.storm@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
+
 defmodule NetworkTest.ASA do
+  import ExUnit.Assertions
+
   defmacro __using__(_opts) do
     quote do
       import NetworkTest.ASA
@@ -14,17 +21,15 @@ defmodule NetworkTest.ASA do
     to: destination
   ) do
     quote do
-      trace = NetworkTest.packet_tracer(
+      assert_allow(
         @device_under_test,
         unquote(input_interface),
         :icmp,
-        unquote(source),
-        unquote(icmp_type),
-        unquote(icmp_code),
-        unquote(destination)
+        type: unquote(icmp_type),
+        code: unquote(icmp_code),
+        from: unquote(source),
+        to: unquote(destination)
       )
-
-      assert trace.result.action == "allow", (trace.result.drop_reason || "")
     end
   end
   defmacro allow(
@@ -36,17 +41,15 @@ defmodule NetworkTest.ASA do
     port: destination_port
   ) do
     quote do
-      trace = NetworkTest.packet_tracer(
+      assert_allow(
         @device_under_test,
         unquote(input_interface),
         unquote(protocol),
-        unquote(source),
-        unquote(source_port),
-        unquote(destination),
-        unquote(destination_port)
+        from: unquote(source),
+        port: unquote(source_port),
+        to: unquote(destination),
+        port: unquote(destination_port)
       )
-
-      assert trace.result.action == "allow", (trace.result.drop_reason || "")
     end
   end
 
@@ -59,17 +62,15 @@ defmodule NetworkTest.ASA do
     to: destination
   ) do
     quote do
-      trace = NetworkTest.packet_tracer(
+      assert_deny(
         @device_under_test,
         unquote(input_interface),
         :icmp,
-        unquote(source),
-        unquote(icmp_type),
-        unquote(icmp_code),
-        unquote(destination)
+        type: unquote(icmp_type),
+        code: unquote(icmp_code),
+        from: unquote(source),
+        to: unquote(destination)
       )
-
-      refute trace.result.action == "allow"
     end
   end
   defmacro deny(
@@ -81,17 +82,101 @@ defmodule NetworkTest.ASA do
     port: destination_port
   ) do
     quote do
-      trace = NetworkTest.packet_tracer(
+      assert_deny(
         @device_under_test,
         unquote(input_interface),
         unquote(protocol),
-        unquote(source),
-        unquote(source_port),
-        unquote(destination),
-        unquote(destination_port)
+        from: unquote(source),
+        port: unquote(source_port),
+        to: unquote(destination),
+        port: unquote(destination_port)
       )
-
-      refute trace.result.action == "allow"
     end
+  end
+
+  def assert_allow(
+    firewall_ip,
+    input_interface,
+    :icmp,
+    type: icmp_type,
+    code: icmp_code,
+    from: source,
+    to: destination
+  ) do
+    trace = NetworkTest.packet_tracer(
+      firewall_ip,
+      input_interface,
+      :icmp,
+      source,
+      icmp_type,
+      icmp_code,
+      destination
+    )
+
+    assert trace.result.action == "allow", (trace.result.drop_reason || "")
+  end
+  def assert_allow(
+    firewall_ip,
+    input_interface,
+    protocol,
+    from: source,
+    port: source_port,
+    to: destination,
+    port: destination_port
+  ) do
+    trace = NetworkTest.packet_tracer(
+      firewall_ip,
+      input_interface,
+      protocol,
+      source,
+      source_port,
+      destination,
+      destination_port
+    )
+
+    assert trace.result.action == "allow", (trace.result.drop_reason || "")
+  end
+
+  def assert_deny(
+    firewall_ip,
+    input_interface,
+    :icmp,
+    type: icmp_type,
+    code: icmp_code,
+    from: source,
+    to: destination
+  ) do
+    trace = NetworkTest.packet_tracer(
+      firewall_ip,
+      input_interface,
+      :icmp,
+      source,
+      icmp_type,
+      icmp_code,
+      destination
+    )
+
+    refute trace.result.action == "allow"
+  end
+  def assert_deny(
+    firewall_ip,
+    input_interface,
+    protocol,
+    from: source,
+    port: source_port,
+    to: destination,
+    port: destination_port
+  ) do
+    trace = NetworkTest.packet_tracer(
+      firewall_ip,
+      input_interface,
+      protocol,
+      source,
+      source_port,
+      destination,
+      destination_port
+    )
+
+    refute trace.result.action == "allow"
   end
 end
